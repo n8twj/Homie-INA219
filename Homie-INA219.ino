f05b2d8dbe5d7650cd457a42383f3e3377396fea
n8twj <mcnamara.jeremy@gmail.com>
2016-09-27 00:27:22 -0400

    update for new version of Homie API

--- Homie-INA219.ino
+++ Homie-INA219.ino
@@ -19,10 +19,10 @@ HomieNode SolarLoadVoltageNode("solarLoadVoltage", "voltage");
 HomieNode SolarCurrentNode("solarCurrent", "millamps");
 
 void setupHandler() {
-  Homie.setNodeProperty(BatteryLoadVoltageNode, "unit", "Volts", true);
-  Homie.setNodeProperty(BatteryCurrentNode, "unit", "millamps", true);
-  Homie.setNodeProperty(SolarLoadVoltageNode, "unit", "Volts", true);
-  Homie.setNodeProperty(SolarCurrentNode, "unit", "millamps", true);
+  Homie.setNodeProperty(BatteryLoadVoltageNode, "unit").setRetained(true).send("volts");
+  Homie.setNodeProperty(BatteryCurrentNode, "unit").setRetained(true).send("millamps");
+  Homie.setNodeProperty(SolarLoadVoltageNode, "unit").setRetained(true).send("volts");
+  Homie.setNodeProperty(SolarCurrentNode, "unit").setRetained(true).send("millamps");
 }
 
 void loopHandler() {
@@ -31,16 +31,16 @@ void loopHandler() {
     currentMa = solar.getCurrent_mA();
     loadVoltage = busVoltage + (shuntVoltage / 1000);
 
-    Homie.setNodeProperty(SolarLoadVoltageNode, "volts", String(loadVoltage), true);
-    Homie.setNodeProperty(SolarCurrentNode, "milliamps", String(currentMa), true);
+    Homie.setNodeProperty(SolarLoadVoltageNode, "volts").send(String(loadVoltage));
+    Homie.setNodeProperty(SolarCurrentNode, "milliamps").send(String(currentMa));
 
     shuntVoltage = battery.getShuntVoltage_mV();
     busVoltage = battery.getBusVoltage_V();
     currentMa = battery.getCurrent_mA();
     loadVoltage = busVoltage + (shuntVoltage / 1000);
 
-    Homie.setNodeProperty(BatteryLoadVoltageNode, "volts", String(loadVoltage), true);
-    Homie.setNodeProperty(BatteryCurrentNode, "milliamps", String(currentMa), true);
+    Homie.setNodeProperty(BatteryLoadVoltageNode, "volts").send(String(loadVoltage));
+    Homie.setNodeProperty(BatteryCurrentNode, "milliamps").send(String(currentMa));
     Homie.prepareForSleep();
 }
 
@@ -54,16 +54,24 @@ void onHomieEvent(HomieEvent event) {
 }
 
 void setup() {
+  Serial.begin(115200);
   battery.begin();
   solar.begin();
-  Homie.setFirmware("ina219", "1.0.1");
-  Homie.registerNode(SolarLoadVoltageNode);
-  Homie.registerNode(SolarCurrentNode);
-  Homie.registerNode(BatteryLoadVoltageNode);
-  Homie.registerNode(BatteryCurrentNode);
+  Homie_setFirmware("ina219", "1.0.1");
   Homie.setSetupFunction(setupHandler);
-  Homie.onEvent(onHomieEvent);
   Homie.setLoopFunction(loopHandler);
+  Homie.onEvent(onHomieEvent);
+
+  BatteryLoadVoltageNode.advertise("unit");
+  BatteryLoadVoltageNode.advertise("voltage");
+  BatteryCurrentNode.advertise("unit");
+  BatteryCurrentNode.advertise("milliamps");
+
+  SolarLoadVoltageNode.advertise("unit");
+  SolarLoadVoltageNode.advertise("voltage");
+  SolarCurrentNode.advertise("unit");
+  SolarCurrentNode.advertise("milliamps");
+  
   Homie.setup();
 }
 
